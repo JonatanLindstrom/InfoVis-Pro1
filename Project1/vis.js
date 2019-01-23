9f7618465f1bb03a16e0affb74e601012a2b3058
Jonatan Lindström <jonatan3@kth.se>
2019-01-23 23:49:22 +0100

    Parcoords with linked table

--- Project1/vis.js
+++ Project1/vis.js
@@ -4,32 +4,46 @@ d3.csv('dataset.csv', function(data) {
         "#fb9a99","#e31a1c","#fdbf6f","#ff7f00",
         "#cab2d6","#6a3d9a","#ffff99","#b15928"]);
 
-    var color = function(d) { return colors(d.group); };
-
     var parcoords = d3.parcoords()("#vis")
-    .data(data)
-    .hideAxis(["name"])
-    .color(color)
-    .alpha(0.25)
-    .composite("darken")
-    .margin({ top: 24, left: 150, bottom: 12, right: 0 })
-    .mode("queue")
-    .render()
-    .brushMode("1D-axes");
+        .data(data)
+        .hideAxis(['Major', 'Degree', 'Interests', 'Goals'])
+        .alpha(0.2)
+        .composite("darken")
+        .margin({ top: 24, left: 150, bottom: 12, right: 0 })
+        .mode("queue")
+        .render()
+        .reorderable()
+        .brushMode("1D-axes");
+
+    var table = d3.select('#list').append('table')
+        .attr('class', 'table table-hover')
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+    
+    thead.append('tr')
+        .selectAll('th')
+            .data(d3.keys(data[0]))
+            .enter()
+        .append('th')
+            .text(function(d) { return d; })
+
+    var rows = tbody.selectAll('tr')
+        .data(data)
+        .enter()
+        .append('tr')
+        .on('mouseover', function(d,i) {
+            parcoords.highlight([data[i]]);
+        })
+        
+    var cells = rows.selectAll('td')
+        .data(function(row) {
+            return d3.keys(data[0]).map(function (column) {
+                return { column: column, value: row[column] }
+            })
+        })
+        .enter()
+        .append('td')
+            .attr('class', 'text-truncate')
+            .attr('style', 'max-width: 150px')
+            .text(function(d) { return d.value })
 })
-
-
-// var data = [
-//     [1, 0, 1, 1, 1],
-//     [2, 2, 1, 2, 0],
-//     [3, 3, 2, 3, 4],
-//     [4, 5, 2, 4, 4],
-//     [1, 2, 3, 5, 4]
-// ];
-
-// var pc = d3.parcoords()('#vis')
-//     .data(data)
-//     .alpha(0.2)
-//     .margin({ top:20, left:0, bottom: 12, right:0 })
-//     .render()
-//     .reorderable();
