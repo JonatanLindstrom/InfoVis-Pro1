9d1e5d4bd3883c9cbc36e3bdf1819eb48f45d628
Jonatan Lindström <jonatan3@kth.se>
2019-01-24 14:57:44 +0100

    GroupSelection into a second table.

--- Project1/vis.js
+++ Project1/vis.js
@@ -1,10 +1,12 @@
 d3.csv('dataset.csv', function(dataset) {
+    //Load data and map it
     var data = d3.entries(dataset).map(function(d) {
         var val = d.value;
         val.key = d.key;
         return val;
     })
 
+    //Create and style parcoords
     var colorScheme = d3.scale.linear()
         .domain([0,8])
         .range(['steelblue', 'brown'])
@@ -14,44 +16,124 @@ d3.csv('dataset.csv', function(dataset) {
 
     var parcoords = d3.parcoords()('#vis')
         .data(data)
-        .hideAxis(['Namn', 'Major', 'Degree', 'Interests', 'Goals', 'Communication', 'Collab', 'Git'])
+        .hideAxis(['Name', 'Major', 'Degree', 'Interests', 'Goals', 'Communication', 'Collab', 'Git', 'key'])
         .color(color)
-        .alpha(0.3)
+        .alpha(0.4)
         .composite('darken')
-        .margin({ top: 24, left: 150, bottom: 12, right: 0 })
+        .margin({ top: 20, left: 20, bottom: 20, right: 10 })
         .mode('queue')
         .render()
         .reorderable()
         .brushMode('1D-axes')
 
-    var table = d3.select('#list').append('table')
-        .attr('class', 'table table-hover')
-    var thead = table.append('thead')
-    var tbody = table.append('tbody')
-    
-    var columns = d3.keys(data[0])
-        columns.splice(1,4)
-        columns.splice(9,3)
+    // var groupcoords = d3.parcoords()('#groupVis')
+    //     .data(groupData)
+    //     .hideAxis(['Name', 'Major', 'Degree', 'Interests', 'Goals', 'Communication', 'Collab', 'Git', 'key'])
+    //     .color(color)
+    //     .alpha(0.4)
+    //     .composite('darken')
+    //     .margin({ top: 20, left: 20, bottom: 20, right: 10 })
+    //     .mode('queue')
+    //     .render()
+    //     .reorderable()
+    //     .brushMode('1D-axes')
 
-    thead.append('tr')
+    //Create table for the group members (the selected students)
+    var groupTable = d3.select('#groupTable').append('table')
+        .attr('class', 'table table-hover')
+    var groupThead = groupTable.append('thead')
+    var groupTbody = groupTable.append('tbody')
+
+    var groupColumns = d3.keys(data[0]).splice(0,1)
+        groupColumns.push('Interests')
+
+        groupThead.append('tr')
         .selectAll('th')
-            .data(columns)
+            .data(groupColumns)
             .enter()
         .append('th')
             .text(function(d) { return d; })
 
-    var rows = tbody.selectAll('tr')
+    var groupRows = groupTbody.selectAll('tr')
+        .data(data)
+        .enter()
+        .append('tr')
+        .attr('style', 'display:none')
+        .on({
+            'mouseover': function(d,i) { parcoords.highlight([data[i]]) },
+            'mouseout': parcoords.unhighlight,
+            'click': function(d,i) { 
+                var selected = data.map(function(d) { return data[i].key })
+                groupTbody.selectAll('tr')
+                    .filter(function(d) { return selected.indexOf(d.key) > -1})
+                    .attr('style', 'display:none')
+            }
+        })
+            
+    var groupCells = groupRows.selectAll('td')
+        .data(function(row) {
+            return groupColumns.map(function (column) {
+                var cellData = row[column]
+                return { 
+                    column: column, 
+                    value: cellData }
+            })
+        })
+        .enter()
+        .append('td')
+            .attr('class', 'text-truncate')
+            .attr('style', 'max-width: 150px')
+            .text(function(d) { 
+                if  (d.column == 'Interests') {
+                    return null
+                }
+                return d.value 
+            })
+            .append('span')
+                .attr('class', 'badge badge-pill badge-warning')
+                .text(function(d) { 
+                    if (d.column == 'Interests') {
+                        return data['IVIS']
+                    }
+                    return null
+                })
+
+
+
+    //Create the table for the entire class (all students)
+    var classTable = d3.select('#classTable').append('table')
+        .attr('class', 'table table-hover')
+    var classThead = classTable.append('thead')
+    var classTbody = classTable.append('tbody')
+    
+    var classColumns = d3.keys(data[0]).splice(0,1)
+        classColumns.push('Interests')
+
+        classThead.append('tr')
+        .selectAll('th')
+            .data(classColumns)
+            .enter()
+        .append('th')
+            .text(function(d) { return d; })
+
+    var classRows = classTbody.selectAll('tr')
         .data(data)
         .enter()
         .append('tr')
         .on({
             'mouseover': function(d,i) { parcoords.highlight([data[i]]) },
-            'mouseout': parcoords.unhighlight
+            'mouseout': parcoords.unhighlight,
+            'click': function(d,i) { 
+                var selected = data.map(function(d) { return data[i].key })
+                groupTbody.selectAll('tr')
+                    .filter(function(d) { return selected.indexOf(d.key) > -1})
+                    .attr('style', 'null')
+            }
         })
         
-    var cells = rows.selectAll('td')
+    var classCells = classRows.selectAll('td')
         .data(function(row) {
-            return columns.map(function (column) {
+            return classColumns.map(function (column) {
                 return { column: column, value: row[column] }
             })
         })
@@ -62,11 +144,18 @@ d3.csv('dataset.csv', function(dataset) {
             .text(function(d) { return d.value })
 
 
+
+    //Filter function for groupTable AND classTable when parcoords is brushed
     parcoords.on('brush', function(items) {
         var selected = items.map(function(d) { return d.key; })
-        tbody.selectAll('tr')
+        classTbody.selectAll('tr')
             .attr('style', 'display: none')
             .filter(function(d) { return selected.indexOf(d.key) > -1 })
             .attr('style', 'null')
+
+        groupTbody.selectAll('tr')
+            .style('opacity', 0.35)
+            .filter(function(d) { return selected.indexOf(d.key) > -1 })
+            .style('opacity', 1)
     })
 })
